fc194a185778a39767553c8f9df37b6ba738e81d
blurdel <5648737+blurdel@users.noreply.github.com>
2023-04-17 19:24:09 -0400

    initial commit

---
+++ src/main/java/com/blurdel/sfgjms/GuruSpringJmsApplication.java
@@ -0,0 +1,13 @@
+package com.blurdel.sfgjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruSpringJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruSpringJmsApplication.class, args);
+	}
+
+}
